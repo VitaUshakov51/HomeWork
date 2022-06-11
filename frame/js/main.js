ad6b2e0a72fede9f3d5f95f5fb3595b3914b54a4
VitaUshakov51 <102901102+VitaUshakov51@users.noreply.github.com>
2022-06-12 00:06:33 +0300

    frame
    frame

---
+++ frame/js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    $('.feature').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 500,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+        ]
+      });
+
+$('.header__burger').on('click' , function(e) {
+  e.preventDefault()
+  $('.header__top').toggleClass('header__top--open')
+  $('.overlay').toggleClass('overlay--show')
+})
+
+    });
