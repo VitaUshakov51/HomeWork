cbff198653bf94486cf262f93d012b92b42b1c7b
VitaUshakov51 <vitavita-98@yandex.ru>
2022-10-11 17:34:29 +0300

    immortals

---
+++ Immortals/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+
+
+$('.burger').on('click' , function(e) {
+  e.preventDefault()
+  $('.header__nav').toggleClass('header__nav--open')
+  $('.overlay').toggleClass('overlay--show')
+})
+
+    });
