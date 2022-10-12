6bac3b537bc8402b5f001532346c56f1ed35d3d8
VitaUshakov51 <vitavita-98@yandex.ru>
2022-10-12 12:47:26 +0300

    51

--- Immortals/js/main.js
+++ Immortals/js/main.js
@@ -9,4 +9,40 @@ $('.burger').on('click' , function(e) {
   $('.overlay').toggleClass('overlay--show')
 })
 
+
+$('.nft__slider').slick({
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  appendDots: $('.nft__dots'),
+  responsive: [
+    {
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 710,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+  ]
+});
+
+$('.nft__arrow-left').on('click', function (e) {
+  e.preventDefault()
+  $('.nft__slider').slick('slickPrev')
+})
+$('.nft__arrow-right').on('click', function (e) {
+  e.preventDefault()
+  $('.nft__slider').slick('slickNext')
+})
+
     });
