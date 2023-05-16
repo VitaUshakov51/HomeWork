a2bb1799d5d438f66a0ba6cd9bffb2ddfe515184
VitaUshakov51 <vitavita-98@yandex.ru>
2023-05-16 17:32:54 +0300

    form

--- jsform/js/main.js
+++ jsform/js/main.js
@@ -2,21 +2,6 @@ window.onload = function () {
     console.log('JavaScript успешно загружен');
 
 
-    let inputFullName = document.getElementById('fullName');
-    let regex = /[0-9]/g;
-    inputFullName.oninput = function () {
-        this.value = this.value.replace(regex, '');
-    };
-
-
-    let inputYourUsername = document.getElementById('yourUsername');
-    inputYourUsername.oninput = function () {
-        this.value = this.value.replace(regex, '');
-        this.value = this.value.replace(',', '');
-        this.value = this.value.replace('.', '');
-    };
-
-
     function clickCheckbox() {
         let checkBox = document.getElementById('checkbox');
         if (checkBox.checked) {
@@ -27,23 +12,59 @@ window.onload = function () {
     };
     document.getElementById('checkbox').onclick = clickCheckbox;
 
+    let error = document.getElementsByClassName('error');
+    let mainItem = document.getElementsByClassName('main__item');
+
+
+    let person = {};
 
     let form = document.querySelector('form');
     form.addEventListener('submit', function (e) {
         e.preventDefault();
-        if (e.target[0].value.length < 1) {
-            alert('Заполните поле Full Name');
-        } else if (e.target[1].value.length < 1) {
-            alert('Заполните поле Your Username');
-        } else if (e.target[2].value.length < 1) {
-            alert('Заполните поле E-mail');
-        } else if (e.target[3].value.length < 8) {
-            alert('Введите пароль не менее 8 символов');
-        } else if (e.target[4].value.length < 1) {
-            alert('Пароли на совпадают');
+        for (let i = 0; i < error.length; i++) {
+            error[i].style.display = 'none';
+        }
+        for (let i = 0; i < mainItem.length; i++) {
+            mainItem[i].style.borderColor = '#C6C6C4';
+        }
+        if (!e.target[0].value.match(/^[А-Я][а-я]+\s*[А-Я][а-я]+$/)) {
+            error[0].style.display = 'block';
+            mainItem[0].style.borderColor = 'red';
+        } else if (!e.target[1].value.match(/^[\w]+$/)) {
+            error[1].style.display = 'block';
+            mainItem[1].style.borderColor = 'red';
+        } else if (!e.target[2].value.match(/^\w+@[a-z]+\.[a-z]+$/)) {
+            error[2].style.display = 'block';
+            mainItem[2].style.borderColor = 'red';
+        } else if (!e.target[3].value.match(/^[A-Za-z0-9].{8,15}$/)) {
+            error[3].style.display = 'block';
+            mainItem[3].style.borderColor = 'red';
+        } else if (e.target[4].value !== e.target[3].value) {
+            error[4].style.display = 'block';
+            mainItem[4].style.borderColor = 'red';
         } else if (!e.target[5].checked) {
-            alert('Вы не поставили галочку в поле согласие на обработку персональных данных');
+            error[5].style.display = 'block';
         } else {
+            person = {
+                fullName: e.target[0].value,
+                yourUserName: e.target[1].value,
+                eMail: e.target[2].value,
+                password: e.target[3].value,
+                repeatPassword: e.target[4].value,
+                accept: e.target[5].checked,
+            };
+
+
+            let clientsArray = [];
+            let clients = localStorage.getItem('clients');
+
+            if (clients) {
+                clientsArray = JSON.parse(clients)
+            }
+
+            clientsArray.push(person);
+            localStorage.setItem('clients', JSON.stringify(clientsArray));
+            // console.log(localStorage);
             form.reset();
             openModal();
         }
@@ -68,8 +89,10 @@ window.onload = function () {
         document.getElementById('itemEmail').remove();
         document.getElementById('itemRepeatPassword').remove();
         document.getElementById('itemCheckBox').remove();
-        document.getElementById('mainLink').remove();
+        document.getElementById('mainLink').innerText = 'Registration';
         document.getElementById('main__button').innerText = 'Sign In';
+        document.getElementById('mainLink').addEventListener("click", ev => window.location.reload())
+        form.reset();
         authForm();
     }
 
@@ -81,16 +104,53 @@ window.onload = function () {
     function authForm() {
         loginBtn.onclick = (e) => {
             e.preventDefault();
-            if (form[0].value.length < 1) {
-                alert('Заполните поле Your Username')
-            } else if (form[1].value.length < 8) {
-                alert('Пароль менее 8 символов');
-            } else {
-                alert('Добро пожаловать, ' + form[0].value + '!');
-                window.location.reload();
+            for (let i = 0; i < error.length; i++) {
+                error[i].style.display = 'none';
             }
+            for (let i = 0; i < mainItem.length; i++) {
+                mainItem[i].style.borderColor = '#C6C6C4';
+            }
+            if (form[0].value.length < 1) {
+                error[0].innerText = 'Поле на заполнено'
+                error[0].style.display = 'block';
+                mainItem[0].style.borderColor = 'red';
+
+            } else if (form[1].value.length < 8) {
+                error[1].innerText = 'Пароль менее 8 символов'
+                error[1].style.display = 'block';
+                mainItem[1].style.borderColor = 'red';
+            } else {
+                userCheck();
+            }
+        }
+    }
+
+
+
+    function userCheck() {
+        let clientsArray = JSON.parse(localStorage.getItem('clients'));
+        let userName = document.getElementById('yourUsername').value;
+        let password = document.getElementById('password').value;
+        if (!clientsArray.find((el) => el.yourUserName === userName)) {
+            error[0].innerText = 'Такой пользователь не найден';
+            error[0].style.display = 'block';
+            mainItem[0].style.borderColor = 'red';
+        } else if (!clientsArray.find((el) => el.password === password)) {
+            error[1].innerText = 'Неверный пароль'
+            error[1].style.display = 'block';
+            mainItem[1].style.borderColor = 'red';
+        } else {
+            document.getElementById('itemUserName').remove();
+            document.getElementById('itemPassword').remove();
+            document.getElementById('mainTitle').innerText = 'Welcome, ' + userName  + ' !';
+            document.getElementById('main__button').innerText = 'Exit';
+            document.getElementById('mainLink').remove();
+            loginBtn.addEventListener('click', function (event) {
+                window.location.reload();
+            })
         }
     }
 
 };
 
+let test = JSON.parse(localStorage.getItem('clients'));
