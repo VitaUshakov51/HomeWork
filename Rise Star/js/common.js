2944b9a00f07c35fc33226dc04c92b9d9cc84c2d
VitaUshakov51 <vitavita-98@yandex.ru>
2023-11-24 15:17:50 +0300

    fix bugs menu

--- Rise Star/js/common.js
+++ Rise Star/js/common.js
@@ -1,13 +1,17 @@
    const headerMenuButton = document.getElementById('headerMenu');
    const menu = document.getElementById('menu');
    const closeMenuButton = document.getElementById('closeMenu');
+   const body = document.getElementById('body');
 
    function showMenu() {
            menu.classList.add('active');
+           body.style.overflow = 'hidden';
+
    };
 
    function closeMenu() {
        menu.classList.remove('active');
+       body.style.removeProperty('overflow');
    };
 
    headerMenuButton.addEventListener('click', showMenu);
