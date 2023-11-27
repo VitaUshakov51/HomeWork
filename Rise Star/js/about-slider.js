70e63c5bed33cf87017ad82e6ee3a309fd6883e7
VitaUshakov51 <vitavita-98@yandex.ru>
2023-11-27 13:56:29 +0300

    add client page

--- Rise Star/js/about-slider.js
+++ Rise Star/js/about-slider.js
@@ -1,34 +1,38 @@
-$('.team__slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    centerPadding: '20px',
-    arrows: false,
-    appendDots: $(".team__arrows"),
-    responsive: [
-        {
-            breakpoint: 1000,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 700,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        },
+(function (){
+    $('.team__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        centerPadding: '20px',
+        arrows: false,
+        appendDots: $(".team__arrows"),
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
 
-    ]
-});
+        ]
+    });
 
-$(".team__arrow-left").click(function () {
-    $(".team__slider").slick("slickPrev")
-})
+    $(".team__arrow-left").click(function () {
+        $(".team__slider").slick("slickPrev")
+    })
 
-$(".team__arrow-right").click(function () {
-    $(".team__slider").slick("slickNext")
-})
+    $(".team__arrow-right").click(function () {
+        $(".team__slider").slick("slickNext")
+    })
+
+})();
+
--- Rise Star/js/common.js
+++ Rise Star/js/common.js
@@ -1,21 +1,25 @@
-   const headerMenuButton = document.getElementById('headerMenu');
-   const menu = document.getElementById('menu');
-   const closeMenuButton = document.getElementById('closeMenu');
-   const body = document.getElementById('body');
+(function (){
+    const headerMenuButton = document.getElementById('headerMenu');
+    const menu = document.getElementById('menu');
+    const closeMenuButton = document.getElementById('closeMenu');
+    const body = document.getElementById('body');
 
-   function showMenu() {
-           menu.classList.add('active');
-           body.style.overflow = 'hidden';
+    function showMenu() {
+        menu.classList.add('active');
+        body.style.overflow = 'hidden';
 
-   };
+    };
 
-   function closeMenu() {
-       menu.classList.remove('active');
-       body.style.removeProperty('overflow');
-   };
+    function closeMenu() {
+        menu.classList.remove('active');
+        body.style.removeProperty('overflow');
+    };
 
-   headerMenuButton.addEventListener('click', showMenu);
-   closeMenuButton.addEventListener('click', closeMenu);
+    headerMenuButton.addEventListener('click', showMenu);
+    closeMenuButton.addEventListener('click', closeMenu);
+
+})();
+
 
 
 
